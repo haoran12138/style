e9cc73db91d70e3419e6b779aba1f494f0c1b19b
haoran <1147604098@qq.com>
2020-08-02 22:04:17 +0800

    设计模式试题

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
-// 惰性单例
-// var getSingle = function (fn) {
-// 	var result;
-// 	return function () {
-// 		return result || (result = fn.apply(this, arguments));
-// 	};
-// };
-
-// function A(name) {
-// 	this.name = name;
-// }
-
-// var createA = getSingle(function (name) {
-// 	return new A(name);
-// });
-
-// var a = createA("a1");
-// var b = createA("a2");
-// console.log(a.name);
-// console.log(b.name);
-// console.log(a === b);
-
-var obj = {
-	car: {
-		mobil: ""
-	},
-	other: [
-		{
-			mobil: ""
+function commodity({ hotMax, notHotMax }) {
+	let hot = window.hot || [];
+	let noHot = window.localStorage.getItem("noHot") || [];
+	async function get(id) {
+		let oprData = [...notHot, ...noHot];
+		let data = oprData.find(item => {
+			return item === id;
+		});
+		if (data) {
+			return data;
+		} else {
+			let res = await getCommodityById(id);
+			add(res);
+			return res;
 		}
-	]
-};
-let key = "car.mobil";
-obj[key.split(".")] = "asdf";
-function setDataFn(path, value, obj) {
-	const arr = path.split(".");
-	const len = arr.length - 1;
-	arr.reduce((cur, key, index) => {
-		if (typeof cur[key] === "undefined") {
-			throw "不存在";
+	}
+
+	function add(data) {
+		let oprData = hot;
+		let oprMax = hotMax;
+		if (!data.isHot) {
+			oprData = noHot;
+			oprMax = notHotMax;
 		}
-		if (index === len) {
-			cur[key] = value;
+		oprData.push(data);
+		oprData.lenght > oprMax && oprData.shift();
+		marge(data.isHot, oprData);
+	}
+	function upd(id, data) {
+		let oprData = hot;
+		if (!data.isHot) {
+			oprData = noHot;
 		}
-		return cur[key];
-	}, obj);
-}
-let key = "car.mobil";
-obj[key.split(".")] = "asdf";
-setDataFn("car.mobil", 1234, obj);
-setDataFn("other.0.mobil", 4312, obj);
-console.log(obj);
 
-// let arr = "car.mobile.aa".split(".");
-// let arg = obj;
-// for (let i = 0; i < arr.length - 1; i++) {
-// 	arg = arg[arr[i]];
-// }
+		let findIndex = oprData.findIndex(item => {
+			return item === id;
+		});
 
-// arg[arr[arr.length - 1]] = "123";
-
-// console.log(obj.car.mobile.aa);
-var obj = {
-	car: {
-		mobile: {
-			aa: ""
+		if (findIndex) {
+			oprData.splice(findIndex, 1, data);
+			marge(data.isHot, oprData);
+		} else {
+			get(id);
 		}
-	},
-	other: [{ mobil: "1234" }]
-};
-
-function f1(path, val, obj) {
-	let keys = path.split(".");
-	let endKeys = keys.pop();
-	keys.forEach((item, index) => {
-		obj = obj[item];
-	});
-	obj[endKeys] = val;
+	}
+	function marge(isHot, oprData) {
+		if (!isHot) {
+			window.localStorage.setItem("noHot", oprData);
+		} else {
+			Window.hot = oprData;
+		}
+	}
+	return {
+		add,
+		upd,
+		get
+	};
 }
 
-f1("car.mobile.aa", 1234, obj);
+function getCommodityById(id) {
+	//... ajax 请求
+}
